ed821eabd6165752371dd68010512a1abdaf2ecc
Fernando Cuellar <fercuellar@gmail.com>
2023-03-20 17:53:56 -0600

    validacion y mensaje de error

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// campos del formulrio
+// Campos del formulrio
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const telefonoInput = document.querySelector('#telefono');
@@ -6,11 +6,48 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
-// user interface
+// User interface
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
-// registrar eventos
+class Citas {
+    constructor() {
+        this.citas = [];
+    };
+};
+
+class UI {
+    imprimirAlerta(mensaje, tipo){
+        //  Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // agregar clase al tipo de error
+        if(tipo == 'error'){
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // agergar al DOM
+
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        // Quitar la alerta despues de 5 seg
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 5000);
+
+    };
+};
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
+// Registrar eventos
 eventListener();
 function eventListener() {
     mascotaInput.addEventListener('input', datosCita);
@@ -19,9 +56,11 @@ function eventListener() {
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 };
 
-// objeto con la informacion de la cita
+// Objeto con la informacion de la cita
 const citaObj = {
     mascota: '',
     propietario: '',
@@ -29,11 +68,28 @@ const citaObj = {
     fecha: '',
     hora: '',
     sintomas: ''
-}
+};
 
-// agrega datos al objeto de cita
+// Agrega datos al objeto de cita
 function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
     
     console.log(citaObj);
+};
+
+// Valida y agrega una nueva cita a la clase de citas
+function nuevaCita(e) {
+    e.preventDefault();
+
+    // Extraer la informacion del Objeto cita
+    const {mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+
+    // Validar
+    if(mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === ''){
+        ui.imprimirAlerta('Todos los campos son obligactorios', 'error');
+
+        return;
+    };
+
+
 };
