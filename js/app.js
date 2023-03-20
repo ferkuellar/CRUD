597fd94eb38f1b70b55297f3e05981e407fddc6b
Fernando Cuellar <fercuellar@gmail.com>
2023-03-20 17:22:21 -0600

    Validacion de campos del formulario

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// campos del formulrio
 const mascotaInput = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const telefonoInput = document.querySelector('#telefono');
@@ -5,15 +6,22 @@ const fechaInput = document.querySelector('#fecha');
 const horaInput = document.querySelector('#hora');
 const sintomasInput = document.querySelector('#sintomas');
 
+// user interface
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
-
+// registrar eventos
 eventListener();
-function eventListener(){
+function eventListener() {
     mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
 };
 
+// objeto con la informacion de la cita
 const citaObj = {
     mascota: '',
     propietario: '',
@@ -23,6 +31,7 @@ const citaObj = {
     sintomas: ''
 }
 
+// agrega datos al objeto de cita
 function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
     
